50aed611d33624363e12338c7585d4d3ce019508
290105 <290105@student.pwr.edu.pl>
2026-04-14 23:56:47 +0200

    Add files via upload

--- lista obecnosci.cpp
+++ lista obecnosci.cpp
@@ -1,122 +1,210 @@
 #include<iostream>
 #include<string>
 using namespace std;
-struct osoba{
+class osoba{
+    private:
     string imie;
     string nazwisko;
     int numerindeksu;
-    bool obecny;
-};
-osoba grupa[10];
-int liczbaosob=0;
-
-void dodajOsobe(osoba* grupa, int &liczbaosob, string imie, string nazwisko, int indeks) {
-    if (liczbaosob < 10) {
-        grupa[liczbaosob].imie = imie;
-        grupa[liczbaosob].nazwisko = nazwisko;
-        grupa[liczbaosob].numerindeksu = indeks;
-        grupa[liczbaosob].obecny = 0;
-        
-        liczbaosob++; 
-    }
-}
-void ustawobecnosc(osoba* grupa, string nazwiskoSprawdzane, int &liczbaosob) {
-    for(int i = 0; i < liczbaosob; i++) {
-        if(grupa[i].nazwisko == nazwiskoSprawdzane) {
-            grupa[i].obecny = 1;
-            break;
-        }
-    }
-}
-void wyswietllisteobecnosci(osoba *grupa, int &liczbaosob) {
-    cout << "\nLista obecnosci" << endl;
-    for(int i = 0; i < liczbaosob; i++) {
-        if(grupa[i].nazwisko != "") {
-            cout << grupa[i].nazwisko << " - " << (grupa[i].obecny ? "1" : "0") << endl;
-        }
-    }
-}
-void edycjadanych(osoba* grupa, int &liczbaosob){
-    string nazwiskodozmienienia;
-    int opcje;
-    cout<<"Podaj nazwisko osoby, ktorej dane chcesz zmienic: ";
-    cin>>nazwiskodozmienienia;
-    for(int i=0; i<liczbaosob; i++){
-        if(grupa[i].nazwisko==nazwiskodozmienienia){
-            cout<<"Co chcesz zmienic: 1.Imie \n2.Nazwisko \n3.Indeks \n4.Obecnosc ";
-            cin>>opcje;
-            if(opcje==1){
-                cout<<"Podaj nowe imie: ";
-                cin>>grupa[i].imie;
-            }
-            else if(opcje==2){
-                cout<<"Podaj nowe nazwisko: ";
-                cin>>grupa[i].nazwisko;
-            }
-            else if(opcje==3){
-                cout<<"Podaj nowy indeks: ";
-                cin>>grupa[i].numerindeksu;
-            }
-            else if(opcje==4){
-                cout<<"Zmien obecnosc: 0 lub 1 ";
-                cin>>grupa[i].obecny;
-            }
-            else{
-                cout<<"Nie ma takiej opcji"<<endl;
-            }
-            return;
-        }
-    }
-      cout<<"Nie znaleziono takiej osoby"<<endl;
-}
-void usuwanieosob(osoba* grupa, int &liczbaosob){
-    string nazwisko;
-    cout<<"Podaj nazwisko osoby, ktora chcesz usunac: ";
-    cin>>nazwisko;
-    for(int i=0; i<liczbaosob; i++){
-        if(grupa[i].nazwisko==nazwisko){
-            grupa[i]=grupa[liczbaosob-1];
-            liczbaosob--;
-            return;
-        }
+    public:
+    string getImie() { 
+        return imie;
      }
-     cout<<"Nie ma takiego nazwiska"<<endl;
-}
+    string getNazwisko() { 
+        return nazwisko;
+     }
+    int getNumerIndeksu() { 
+        return numerindeksu; 
+    }
+    void setImie(string noweImie) { 
+        imie = noweImie; 
+    }
+    void setNazwisko(string noweNazwisko) {
+         nazwisko = noweNazwisko; 
+        }
+    void setNumerIndeksu(int nowyNumerIndeksu) { 
+        numerindeksu = nowyNumerIndeksu;
+     }
+};
+class ListaObecnosci{
+    private:
+    osoba* grupa[10];
+    int obecny[10];
+    int liczbaosob=0;
+    public:
+    void dodajOsobe(osoba* nowaosoba) {
+        if (liczbaosob < 10) {
+            grupa[liczbaosob]=nowaosoba;
+            obecny[liczbaosob]=0;
+            liczbaosob++; 
+        }
+    }
+    void ustawobecnosc(string nazwiskoSprawdzane) {
+        for(int i = 0; i < liczbaosob; i++) {
+            if(grupa[i]->getNazwisko() == nazwiskoSprawdzane) {
+                obecny[i] = 1;
+                break;
+            }
+        }
+    }
+    void wyswietllisteobecnosci() {
+        cout << "\nLista obecnosci" << endl;
+        for(int i = 0; i < liczbaosob; i++) {
+            if(grupa[i] != nullptr) {
+                cout << grupa[i]->getNazwisko() << " - " << (obecny[i] ? "1" : "0") << endl;
+            }
+        }
+    }
+    void edycjadanych(){
+        string nazwiskodozmienienia, imie, nazwisko;
+        int indeks, opcje;
+        cout<<"Podaj nazwisko osoby, ktorej dane chcesz zmienic: ";
+        cin>>nazwiskodozmienienia;
+        for(int i=0; i<liczbaosob; i++){
+            if(grupa[i]->getNazwisko()==nazwiskodozmienienia){
+                cout<<"Co chcesz zmienic: 1.Imie \n2.Nazwisko \n3.Indeks \n4.Obecnosc ";
+                cin>>opcje;
+                if(opcje==1){ 
+                    cout<<"Podaj nowe imie: ";
+                     cin>>imie;
+                      grupa[i]->setImie(imie);
+                     }
+                else if(opcje==2){ 
+                    cout<<"Podaj nowe nazwisko: ";
+                     cin>>nazwisko; 
+                     grupa[i]->setNazwisko(nazwisko);
+                     }
+                else if(opcje==3){
+                     cout<<"Podaj nowy indeks: "; 
+                     cin>>indeks; grupa[i]->setNumerIndeksu(indeks);
+                     }
+                else if(opcje==4){ 
+                    cout<<"Zmien obecnosc: 0 lub 1 ";
+                     cin>>obecny[i]; 
+                    }
+                else{ 
+                    cout<<"Nie ma takiej opcji"<<endl;
+                 }
+                return;
+            }
+        }
+        cout<<"Nie znaleziono takiej osoby"<<endl;
+    }
+    void usuwanieosob(){
+        string nazwisko;
+        cout<<"Podaj nazwisko osoby, ktora chcesz usunac: ";
+        cin>>nazwisko;
+        for(int i=0; i<liczbaosob; i++){
+            if(grupa[i]->getNazwisko()==nazwisko){
+                grupa[i]=grupa[liczbaosob-1];
+                obecny[i]=obecny[liczbaosob-1];
+                liczbaosob--;
+                return;
+            }
+        }
+        cout<<"Nie ma takiego nazwiska"<<endl;
+    }  
+};
 int main() {
     int wybor;
+    osoba katalog[10];
+    int liczbaosob = 0;
+    ListaObecnosci listy[5];
+    int liczbaList = 0;
+    int aktywnaLista = -1;
     do {
-       cout<<"1-Dodaj osobe"<<endl;
-       cout<<"2-Sprawdz obecnosc-podaj nazwisko"<<endl;
-       cout<<"3-Wyswietl liste obecnosci"<<endl;
-       cout<<"4-Edycja danych"<<endl;
-       cout<<"5-Usuwanie osoby"<<endl;
-       cout<<"0-Wyjscie"<<endl;
-       cout<<"Wybierz opcje: ";
-         cin>>wybor;
+        cout<<"1-Dodaj osobe do katalogu"<<endl;
+        cout<<"2-Utworz nowa liste"<<endl;
+        cout<<"3-Wybierz liste"<<endl;
+        cout<<"4-Dodaj osobe z katalogu na liste"<<endl;
+        cout<<"5-Sprawdz obecnosc"<<endl;
+        cout<<"6-Wyswietl liste obecnosci"<<endl;
+        cout<<"7-Edycja danych"<<endl;
+        cout<<"8-Usuwanie osoby z listy"<<endl;
+        cout<<"0-Wyjscie"<<endl;
+        if(aktywnaLista >= 0)
+            cout<<"(aktywna lista: "<<aktywnaLista+1<<")"<<endl;
+        cout<<"Wybierz opcje: ";
+        cin>>wybor;
         
-         if (wybor == 1) {
-             string imie, nazwisko;
-             int indeks;
-             cout << "Podaj imie: "; cin >> imie;
-             cout << "Podaj nazwisko: "; cin >> nazwisko;
+        if (wybor == 1) {
+            string imie, nazwisko;
+            int indeks;
+            cout << "Podaj imie: "; cin >> imie;
+            cout << "Podaj nazwisko: "; cin >> nazwisko;
             cout << "Podaj indeks: "; cin >> indeks;
-             dodajOsobe(grupa, liczbaosob, imie, nazwisko, indeks);
+            katalog[liczbaosob].setImie(imie);
+            katalog[liczbaosob].setNazwisko(nazwisko);
+            katalog[liczbaosob].setNumerIndeksu(indeks);
+            liczbaosob++;
+            cout<<"Dodano do katalogu."<<endl;
         }
         else if (wybor == 2) {
+            if(liczbaList < 5){
+                liczbaList++;
+                aktywnaLista = liczbaList-1;
+                cout<<"Utworzono liste nr "<<liczbaList<<endl;
+            } else {
+                cout<<"Osiagnieto limit list."<<endl;
+            }
+        }
+        else if (wybor == 3) {
+            cout<<"Dostepne listy: 1-"<<liczbaList<<". Wybierz: ";
+            int nr; cin>>nr;
+            if(nr >= 1 && nr <= liczbaList){
+                aktywnaLista = nr-1;
+                cout<<"Wybrano liste "<<nr<<endl;
+            } else {
+                cout<<"Nieprawidlowy numer."<<endl;
+            }
+        }
+        else if (wybor == 4) {
+            if(aktywnaLista < 0){ 
+                cout<<"Najpierw wybierz liste (opcja 3)."<<endl; 
+                continue; 
+            }
+            cout<<"Osoby w katalogu:"<<endl;
+            for(int i=0; i<liczbaosob; i++)
+                cout<<i+1<<". "<<katalog[i].getNazwisko()<<" "<<katalog[i].getImie()<<endl;
+            cout<<"Wybierz numer osoby: ";
+            int nr; 
+            cin>>nr;
+            if(nr >= 1 && nr <= liczbaosob){
+                listy[aktywnaLista].dodajOsobe(&katalog[nr-1]);
+                cout<<"Dodano na liste."<<endl;
+            }
+        }
+        else if (wybor == 5) {
+            if(aktywnaLista < 0){ 
+                cout<<"Najpierw wybierz liste."<<endl;
+                 continue; 
+                }
             string nazwiskoDoSprawdzenia;
             cout << "Podaj nazwisko osoby, ktora jest obecna: ";
             cin >> nazwiskoDoSprawdzenia;
-            ustawobecnosc(grupa, nazwiskoDoSprawdzenia, liczbaosob);
+            listy[aktywnaLista].ustawobecnosc(nazwiskoDoSprawdzenia);
         } 
-        else if (wybor == 3) {
-            wyswietllisteobecnosci(grupa, liczbaosob);
+        else if (wybor == 6) {
+            if(aktywnaLista < 0){ 
+                cout<<"Najpierw wybierz liste."<<endl; 
+                continue; 
+            }
+            listy[aktywnaLista].wyswietllisteobecnosci();
         } 
-        else if(wybor==4) {
-                edycjadanych(grupa, liczbaosob);
-            }
-         else if(wybor==5) {
-                usuwanieosob(grupa, liczbaosob);
-            }
+        else if(wybor==7) {
+            if(aktywnaLista < 0){ 
+                cout<<"Najpierw wybierz liste."<<endl;
+                 continue;
+                 }
+            listy[aktywnaLista].edycjadanych();
+        }
+        else if(wybor==8) {
+            if(aktywnaLista < 0){ 
+                cout<<"Najpierw wybierz liste."<<endl;
+                 continue;
+                 }
+            listy[aktywnaLista].usuwanieosob();
+        }
         else if (wybor == 0) {
             cout << "Koniec programu." << endl;
         }
